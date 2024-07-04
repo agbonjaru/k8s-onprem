14b9f74993c9f9fa6cea4900d164e40d8bb15ff9
agbonjaru <jstryngs@gmail.com>
2024-07-04 21:17:29 +0100

    Update worker-node.sh
    Added current user to docker group

--- worker-node.sh
+++ worker-node.sh
@@ -5,8 +5,16 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
 # Add Docker GPG key and repository
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y docker-ce
+
+#add current user to docker group
+sudo usermod -aG docker ${USER}
+newgrp docker
 
 # Install Docker
 sudo apt-get update
