624ae48ab2463af13b3bc9fe90774b88e9e15961
agbonjaru <jstryngs@gmail.com>
2024-07-26 04:29:25 +0100

    Update master-node.sh

--- master-node.sh
+++ master-node.sh
@@ -36,6 +36,12 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 #Configure containerd so that it starts using systemd as cgroup.
+sudo curl -fsSL https://download.docker.com/linux/ubu... | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+#Now, install containerd
+
+sudo apt update && sudo apt install -y runc containerd.io 
 
 containerd config default | sudo tee /etc/containerd/config.toml 
 sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
